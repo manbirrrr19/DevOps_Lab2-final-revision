1130804b8f3609b6278d837ec37062f4255dbd15
Manbir <manbirjit.togar@gmail.com>
2023-11-27 22:21:36 +0800

    Add values to bmi.py

--- bmi.py
+++ bmi.py
@@ -5,9 +5,14 @@ def calculate_bmi(height, weight):
     BMI = round(BMI, 2)
     if BMI < 18.5:
         print("Your BMI is ", BMI, ", underweight")
+        value = -1
     elif BMI >= 18.5 and BMI <= 25:
         print("Your BMI is ", BMI, ", normal weight")
+        value = 0
     else:
         print("Your BMI is ", BMI, ", overweight")
+        value = 1
+
+    return value
 
 calculate_bmi(weight=57, height=1.73)
